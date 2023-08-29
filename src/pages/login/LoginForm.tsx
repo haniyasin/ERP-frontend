2574a6ab999d007cf0e53a33517c241e8087edb0
matejjj27 <matej4o9@gmail.com>
2023-08-29 22:24:40 +0300

    add marginTop so that everything is below the header

--- src/pages/login/LoginForm.tsx
+++ src/pages/login/LoginForm.tsx
@@ -46,7 +46,15 @@ const LoginForm = () => {
           </Stack>
         </Grid>
         <Grid item container direction="column" lg={4}>
-          <img src={logo} alt="logo" />
+          <img
+            src={logo}
+            alt="logo"
+            style={{
+              width: "80%",
+              borderRadius: "200px",
+              margin: "auto"
+            }}
+          />
         </Grid>
       </Grid>
     </Form>
